3383563f90dfd73adf532bf5c31d6fe00c06096d
Alexandr Binkovski <alexbine@gmail.com>
2024-01-11 22:29:02 +0200

    Add Whisper installation

---
+++ prepare-env/06-whisper.ps1
@@ -0,0 +1,2 @@
+echo "installing whisper"
+pip install git+https://github.com/openai/whisper.git 
--- prepareEnv.ps1
+++ prepareEnv.ps1
@@ -1,32 +1,9 @@
-##PowerShell -NoExit "script.ps1"
-
-##call prepare-env/01-scoop.bat
-##call prepare-env/02-python-install.bat
-##call prepare-env/03-pip-upd.bat
-##call prepare-env/04-pyTorch-cpu.bat
-##call prepare-env/05-ffmpeg.bat
-
-
-##PowerShell -NoExit "prepare-env/01-scoop.ps1"
-##PowerShell -NoExit "prepare-env/02-python-install.ps1"
-##PowerShell -NoExit "prepare-env/03-pip-upd.ps1"
-##PowerShell -NoExit "prepare-env/04-pyTorch-cpu.ps1"
-##PowerShell -NoExit "prepare-env/05-ffmpeg.ps1"
-
-##set-executionpolicy unrestricted
-
-##PowerShell "prepare-env/01-scoop.ps1"
-##PowerShell "prepare-env/02-python-install.ps1"
-##PowerShell "prepare-env/03-pip-upd.ps1"
-##PowerShell "prepare-env/04-pyTorch-cpu.ps1"
-##PowerShell "prepare-env/05-ffmpeg.ps1"
-
-
 cd prepare-env
 ./01-scoop.ps1
 ./02-python-install.ps1
 ./03-pip-upd.ps1
 ./04-pyTorch-cpu.ps1
 ./05-ffmpeg.ps1
+./06-whisper.ps1
 
 Pause
