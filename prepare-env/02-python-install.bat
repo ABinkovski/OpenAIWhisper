15ba3601309f5fa4b64f875955e4945ce2729512
Alexandr Binkovski <alexbine@gmail.com>
2024-01-11 22:14:05 +0200

    fix install scripts

--- prepare-env/02-python-install.bat
+++ prepare-env/02-python-install.bat
@@ -1,2 +1,9 @@
+echo "add old versions to scoop index"
+scoop bucket add versions
+
+echo "verify python versions"
+scoop search python
+
 echo "installing python "
-scoop install versions/python39
+#scoop install versions/python39
+scoop install python@3.9.13
